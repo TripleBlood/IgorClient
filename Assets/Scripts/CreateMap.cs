5b496916c9f63b1aa417b94179618e9b09798065
TheAbyssWalker23 <v.mikhailov23@gmail.com>
2020-03-19 14:18:17 +0300

    Added script for creating Map Coord Grid

---
+++ Assets/Scripts/CreateMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Models;
+using UnityEngine;
+
+public class CreateMap : MonoBehaviour
+{
+    private Map map;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        map = new Map(3, 3, 3, -1.5f * (float) Math.Sqrt(3), 0, -1.5f);
+        Debug.Log("Map created");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
